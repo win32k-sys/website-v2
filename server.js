b52e0b9911b799b54d4327501b7b0868d819fede
Nate <yt.nater.2017@gmail.com>
2023-08-26 20:55:10 -0700

    / directory

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/games', (req, res) => {
-    res.sendFile(__dirname + '/public/g.html');
+    res.sendFile(__dirname + '/public/src/games.html');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/src/home.html');
 });
 
 app.get('/favicon.ico', (req, res) => {
