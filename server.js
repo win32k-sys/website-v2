ce8ee5ee94dfc06bc51429c7edd934c0d9a90d04
Nate <yt.nater.2017@gmail.com>
2023-08-26 21:38:57 -0700

    server.js

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.get('/home', (req, res) => {
     res.sendFile(__dirname + '/public/src/home.html');
 });
 
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile(__dirname + '/public/src/favicon.ico');
-});
-
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
