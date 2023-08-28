ed20932df1307be68cc613c116295f95293de038
Nate <yt.nater.2017@gmail.com>
2023-08-28 16:29:35 -0700

    fix?

--- glitch.js
+++ glitch.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const app = express();
-app.use(router);
-
 router.get('/games', (req, res) => {
     res.sendFile(__dirname + '/public/games.html');
 });
@@ -19,9 +16,6 @@ router.get('/home', (req, res) => {
 router.get('/load', (req, res) => {
     res.sendFile(__dirname + '/public/loader.html');
 });
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/public/404.html');
-  });
+  
 
 module.exports = router;
