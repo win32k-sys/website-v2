1734af5e56e367565a22905b7300c2ec303eddeb
Nate <yt.nater.2017@gmail.com>
2023-08-28 16:16:47 -0700

    trying to fix issue

--- routes.js
+++ routes.js
@@ -16,10 +16,6 @@ router.get('/home', (req, res) => {
 router.get('/load', (req, res) => {
     res.sendFile(__dirname + '/public/loader.html');
 });
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/public/404.html');
-  });
   
 
 module.exports = router;
