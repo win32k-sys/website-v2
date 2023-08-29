f0b1e1cd43bfe5186026d29086a9beba162bb892
Nate <yt.nater.2017@gmail.com>
2023-08-29 16:59:36 -0700

    f

--- public/js/request.js
+++ public/js/request.js
@@ -1,2 +1,13 @@
-const path = window.location.pathname;
-document.getElementById('path').textContent = path;
+document.addEventListener('DOMContentLoaded', function () {
+    let path = window.location.pathname;
+  
+    if (window !== window.parent) {
+      path = window.parent.location.pathname;
+    }
+  
+    document.getElementById('path').textContent = path;
+  });
+  
+
+  // const path = window.location.pathname;
+// document.getElementById('path').textContent = path;
