a4cbd9c46486bed9836e6d3d2431021898ecaf9c
Nate <yt.nater.2017@gmail.com>
2023-08-29 17:01:52 -0700

    GET

---
+++ public/js/GET.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const queryString = window.location.search.substring(1);
+
+    const pageMappings = {
+        "flappybird": "https://nate-games.github.io/0/g/flappybird/game/",
+        "stickmanhook": "https://nate-games.github.io/0/g/stickmanhook/game/",
+        "doodlejump": "https://nate-games.github.io/0/g/doodle-jump/game/",
+        "404": "/404",
+    };
+
+    const filePath = pageMappings[queryString];
+
+    if (filePath) {
+        document.getElementById("content").src = filePath;
+    } else {
+        window.location.href = "/load?404";
+    }
+});
