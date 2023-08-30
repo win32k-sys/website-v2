aed28f85f8bb262fa5a5823efc55f5c993495d3e
Nate <yt.nater.2017@gmail.com>
2023-08-30 16:16:09 -0700

    +5

--- public/js/GET.js
+++ public/js/GET.js
@@ -3,13 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const pageMappings = {
         "flappybird": "https://nate-games.github.io/0/g/flappybird/game/",
+        "crossyroad": "https://nate-games.github.io/0/g/CrossyRoad/game/",
         "stickmanhook": "https://nate-games.github.io/0/g/stickmanhook/game/",
         "doodlejump": "https://nate-games.github.io/0/g/doodle-jump/game/",
         "geometrydash": "https://cdn.nate-games.workers.dev/?url:https://nate-games.github.io/0/g/geodash",
         "ovo": "https://nate-games.github.io/0/g/ovo/game/",
         "ovo2": "https://nate-games.github.io/0/g/ovo2/game/",
         "ovo3": "https://nate-games.github.io/0/g/ovo3/game/",
+        "plants-vs-zombies": "https://nate-games.github.io/0/g/pvz/game/default/",
+        "fruitninja": "https://nate-games.github.io/0/g/fruitninja/game/",
         "404": "/404",
+        "null": "/js/null.txt",
     };
 
     const filePath = pageMappings[queryString];
