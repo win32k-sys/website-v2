4f9ff7b30cb9bbe33193d4d4d7a61ab3b1520d39
Nate <yt.nater.2017@gmail.com>
2023-09-02 09:11:32 -0700

    json

---
+++ public/js/games-json.js
@@ -0,0 +1,33 @@
+fetch('/json/games.json')
+.then(response => response.json())
+.then(data => {
+    const gameContainer = document.getElementById('game-container');
+
+    data.forEach(game => {
+        const gameCard = document.createElement('div');
+        gameCard.classList.add('game-card');
+        gameCard.onclick = function() {
+            window.location.href = game.url;
+        };
+
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+
+        const img = document.createElement('img');
+        img.src = game.imageSrc;
+        img.onerror = function() {
+            this.src = '/image-placeholder.svg';
+        };
+
+        const p = document.createElement('p');
+        p.textContent = game.name;
+
+        imageContainer.appendChild(img);
+        gameCard.appendChild(imageContainer);
+        gameCard.appendChild(p);
+        gameContainer.appendChild(gameCard);
+    });
+})
+.catch(error => {
+    console.error('Error loading JSON data:', error);
+});
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const routes = require('./routes');
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port} http://localhost:3000`);
 });
