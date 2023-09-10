7c1ea0c737564d877e1f3a30b648a4687299d17e
Nate <yt.nater.2017@gmail.com>
2023-09-10 08:47:30 -0700

    add 7 games

--- routes.js
+++ routes.js
@@ -16,6 +16,10 @@ router.get("/game", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "game-fullscreen.html"));
 });
 
+router.get("/browse", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "game.html"));
+});
+
 router.get('/', (req, res) => {
   res.redirect('/home');
 });
