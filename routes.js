952c365dbaa69a6963aea4f1414973e0388e9997
Nate <yt.nater.2017@gmail.com>
2023-09-09 17:39:43 -0700

    fix

--- routes.js
+++ routes.js
@@ -8,4 +8,24 @@ router.get("/search", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "uv.html"));
 });
 
+router.get("/games", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "games.html"));
+});
+
+router.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "game.html"));
+});
+
+router.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+router.use((req, res) => {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
 export default router;
